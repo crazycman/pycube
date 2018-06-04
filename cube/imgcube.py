596c23d513f4f0fed3072fe9b39b9e4880c260ca
Conrad Rau <conradrau@gmail.com>
2018-06-04 17:32:35 +0200

    Remove useless comment

--- cube/imgcube.py
+++ cube/imgcube.py
@@ -4,4 +4,4 @@ import cv2
 img = cv2.imread("/home/conrad/workspace/pycube/resources/pics/Abrade.jpg", cv2.IMREAD_GRAYSCALE)
 cv2.imshow("frame", img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()  # some useless comment
+cv2.destroyAllWindows()
